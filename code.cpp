d820d041211d1b9f3ccbe4e088d332db126050f7
MemoryTing <44110712+MemoryTing@users.noreply.github.com>
2019-01-17 22:03:43 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -30,44 +30,76 @@ class Student{
 						sub[i][j] = Max[i][j] - Record[i][j];
 					}
 				}
-			//
-				int next_steps[5][6] , next_next_step[5][6];
-				int the_biggest=0 , count=0;
+				int next_step[5][6];
+				int biggest=0 , next_x , next_y;
 				for (int i=0 ; i<5 ; i++){
 					for (int j=0 ; j<6 ; j++){
-						if(color[i][j] == inputColor || color[i][j] == White){
-							next_steps[i][j] = stil_have_how_many_steps_(0, i , j , sub , color , inputColor);
-							next_next_step[i][j] = stil_have_how_many_steps_(1, i , j , sub , color , inputColor);
-							next_steps[i][j] = next_steps[i][j] - next_next_step[i][j];
-							if(next_steps[i][j] > the_biggest) {
-								x = i;
-								y = j;
-								the_biggest = next_steps[i][j];
-								count = 1;
+						if(color[i][j] == White || color[i][j] == inputColor){
+							int now_sub[5][6];
+							Color now_color[5][6];
+							for (int a=0 ; a<5 ; a++){
+								for (int b=0 ; b<6 ; b++){
+									now_sub[a][b] = sub[a][b];
+									now_color[a][b] = color[a][b];
+								}
 							}
-							else if(the_biggest == next_steps[i][j]) count++;
+							int p , q;
+							go_through(i , j , now_sub , now_color , inputColor);//next
+							step(now_sub , now_color , colorrr(inputColor));
+							go_through(x , y , now_sub , now_color , colorrr(inputColor));//oppo_next
+							step(now_sub , now_color , inputColor);
+							go_through(x , y , now_sub , now_color , inputColor);//next_next
+							step(now_sub , now_color , colorrr(inputColor));
+							go_through(x , y , now_sub , now_color , colorrr(inputColor));//oppo_next_next
+							int my_step = 0;
+							for (int a=0 ; a<5 ; a++){
+								for (int b=0 ; b<6 ; b++){
+									if(now_color[a][b] == inputColor || now_color[a][b] == White){
+										my_step += now_sub[a][b];
+									}
+								}
+							}
+							next_step[i][j] = my_step;
+							if(my_step > biggest){
+								biggest = my_step;
+								next_x = i;
+								next_y = j;
+							}
 						}
-						else next_steps[i][j] = 100;
 					}
 				}
-				if(count > 1){
-					for (int i=x ; i<5 ; i++){
+				x = next_x;
+				y = next_y;
+				return;
+        	}
+    	}
+        // Any Code You Want to Add
+        void step(int sub[5][6] , Color color[5][6] , Color inputColor){
+        	int next_steps[5][6] , next_next_step[5][6];
+			int the_biggest=0 , count=0;
+			for (int i=0 ; i<5 ; i++){
+				for (int j=0 ; j<6 ; j++){
+					if(color[i][j] == inputColor || color[i][j] == White){
+						next_steps[i][j] = stil_have_how_many_steps_(0, i , j , sub , color , inputColor);
+						next_next_step[i][j] = stil_have_how_many_steps_(1, i , j , sub , color , inputColor);
+						next_steps[i][j] = next_steps[i][j] - next_next_step[i][j];
+						if(next_steps[i][j] > the_biggest) {
+							x = i;
+							y = j;
+							the_biggest = next_steps[i][j];
+							count = 1;
+						}
+						else if(the_biggest == next_steps[i][j]) count++;
+					}
+					else next_steps[i][j] = 100;
+				}
+			}
+			if(count > 1){
+				for (int i=0 ; i<5 ; i++){
+					if(count == 0) break;
+					for (int j=0 ; i<6 ; i++){
 						if(count == 0) break;
-						for (int j=y ; i<6 ; i++){
-							if(count == 0) break;
-							if(next_steps[i][j] == the_biggest && check(i , j , color , inputColor , sub)){
-								x = i;
-								y = j;
-								return;
-							}
-						}
-					}
-				}
-				else if(count == 1) return;
-				
-				for(int i=4 ; i>-1 ; i--){
-					for (int j=5 ; j>-1 ; j--){
-						if(color[i][j] == inputColor){
+						if(next_steps[i][j] == the_biggest && check(i , j , color , inputColor , sub)){
 							x = i;
 							y = j;
 							return;
@@ -75,8 +107,19 @@ class Student{
 					}
 				}
 			}
-        }
-        // Any Code You Want to Add
+			else if(count == 1) return;
+			
+			for(int i=4 ; i>-1 ; i--){
+				for (int j=5 ; j>-1 ; j--){
+					if(color[i][j] == inputColor){
+						x = i;
+						y = j;
+						return;
+					}
+				}
+			}
+		}
+        
 		Color colorrr(Color mine){
         	if(mine == Blue) return Red;
         	else return Blue;
